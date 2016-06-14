e6f3016f7fb9b597561442b6bfc5feb3bb7c07ba
Luis Pujols <luispujols@hotmail.com>
2016-06-14 13:38:00 -0400

    ADD "Added a basic search page that displays all the users currently registered, this page will show in the future only all the tutors we have avaible so students can select one, also made it login required."

--- quicktutorsApp/urls.py
+++ quicktutorsApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.undercons_page, name='undercons_page'),
     url(r'^home/$', views.home_page, name='home_page'),
+    url(r'^search/$', views.search_page, name='search_page'),
 ]
--- quicktutorsApp/views.py
+++ quicktutorsApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -7,3 +9,8 @@ def undercons_page(request):
 
 def home_page(request):
     return render(request, 'quicktutorsApp/home_page.html')
+
+@login_required()
+def search_page(request):
+    users = User.objects.all()
+    return render(request, 'quicktutorsApp/search_page.html', {'users': users})
