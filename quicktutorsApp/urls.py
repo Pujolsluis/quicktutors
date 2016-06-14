fbeafa2a427dcca700717982680ba6412f2d4ea7
Luis Pujols <luispujols@hotmail.com>
2016-06-14 11:50:11 -0400

    UPDATE "Renamed the home_page to undercons_page so everything makes sense and it allows us to develop the home_page with out any name collisions in the future."

--- quicktutorsApp/urls.py
+++ quicktutorsApp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.undercons_page, name='undercons_page'),
 ]
--- quicktutorsApp/views.py
+++ quicktutorsApp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_page(request):
-    return render(request, 'quicktutorsApp/home_page.html')
+def undercons_page(request):
+    return render(request, 'quicktutorsApp/undercons_page.html')
+
