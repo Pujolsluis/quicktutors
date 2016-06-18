3bb4df96540def57b2784850c6d02f5021ba14fc
Luis Pujols <luispujols@hotmail.com>
2016-06-18 01:43:48 -0400

    UPDATE "Solved the career field, and university field appearance in the update page. only left to solve is Subjects field."

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -5,4 +5,4 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        exclude = ('user', 'career', 'university', 'subjects')
+        exclude = ('user','subjects',)
