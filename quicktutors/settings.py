8d10821b7557fffe06fc7cea9baea6cd7987f55f
Luis Pujols <luispujols@hotmail.com>
2016-06-20 23:49:33 -0400

    ADD "added the context processor of postman so we can use the postman_unread_count context variable in our templates."

--- quicktutors/settings.py
+++ quicktutors/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'postman.context_processors.inbox',
             ],
         },
     },
