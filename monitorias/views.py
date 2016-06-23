094aba35ac1b7725044850a685b29d58950dcc5d
Luis Pujols <luispujols@hotmail.com>
2016-06-23 18:29:44 -0400

    ADD "Created the urls, views and templates, for the monitorias app. also made the seccion list view to display the history of all the secciones de monitoria"

--- monitorias/views.py
+++ monitorias/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from monitorias.models import SeccionMonitoria
 # Create your views here.
+
+def secciones_list(request):
+    secciones = SeccionMonitoria.objects.all()
+    return render(request, 'monitorias/secciones_list.html', {'secciones': secciones})
