af0b3f97db166a01e169839be57e5eb70d4e31da
Luis Pujols <luispujols@hotmail.com>
2016-06-24 18:29:25 -0400

    ADD "Added the Added the secciones_list and filtered it to show all the secciones for the user in which he was either a student or a tutor."

--- monitorias/urls.py
+++ monitorias/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.secciones_list, name='secciones_list'),
     url(r'^new/$', views.secciones_new, name='secciones_new'),
+    url(r'^seccion/(?P<pk>\d+)/$', views.secciones_detail, name='secciones_detail'),
 ]
 
--- monitorias/views.py
+++ monitorias/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from monitorias.models import SeccionMonitoria
 from monitorias.forms import SeccionMonitoriaForm
 from django.shortcuts import redirect
 # Create your views here.
 
 def secciones_list(request):
-    secciones = SeccionMonitoria.objects.all()
+    # import ipdb
+    # ipdb.set_trace()
+    secciones = set()
+    # integrales = SeccionMonitoria.objects.filter(subject__name="Calculo Integral")
+    # integralesSubject = integrales[0].subject
+    for i in SeccionMonitoria.objects.all():
+        if i.estudiante == request.user or i.tutor == request.user:
+            secciones.add(i)
+
     return render(request, 'monitorias/secciones_list.html', {'secciones': secciones})
+
+def secciones_detail(request,pk):
+    seccion = get_object_or_404(SeccionMonitoria, pk=pk)
+    return render(request, 'monitorias/secciones_detail.html', {'seccion': seccion})
 
 def secciones_new(request):
     if request.method == "POST":
