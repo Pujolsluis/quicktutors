356396a95045656578077feaa9f2cdc91753b3c2
Luis Pujols <luispujols@hotmail.com>
2016-06-25 19:15:30 -0400

    UPDATE "Updated the user profile template so it only displays the edit profile button, to the owner of the profile or the staff members so when normal users that are just looking for a tutor. are not able to edit their profile."

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^profile/$', views.personal_profile, name='profile'),
-    url(r'^profile/(?P<profile_id>\d+)$', views.profile),
+    url(r'^profile/(?P<profile_id>\d+)$', views.profile, name='tutor_profile'),
     url(r'^update_profile/$', views.update_profile, name='update_profile'),
     url(r'^send_update_profile/$', views.send_update_profile),
     url(r'^search/$', views.profile_search_page, name='search_page'),
