c4bb75e93a02037569da30a8ff4847a970e3baf7
Luis Pujols <luispujols@hotmail.com>
2016-06-27 16:06:28 -0400

    ADD "Added the comments to the question template, now you can see the number of comments in each question."

--- quickfireQuestions/admin.py
+++ quickfireQuestions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Comment
 
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register(Comment)
