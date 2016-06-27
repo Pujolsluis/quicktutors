1defdc213443ff6756eb811f8b24551f48bdfcf1
Luis Pujols <luispujols@hotmail.com>
2016-06-27 11:41:14 -0400

    ADD "registered the quickFireQuestion app in the admin, and also added the subject field to the questions."

--- quickfireQuestions/models.py
+++ quickfireQuestions/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
-
+from user_profile.models import Subject
 # Create your models here.
 
+defaultSubject = Subject.objects.get(name="Algebra y Geometria Analitica")
 
 class Question(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    subject = models.ForeignKey(Subject, null=True)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
