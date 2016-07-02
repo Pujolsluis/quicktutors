65d0b8d363081dbd3f5ceec8566307cfde25c332
Luis Pujols <luispujols@hotmail.com>
2016-07-02 18:00:16 -0400

    ADD "created the publicities list in the context processors, and now you can use the list of publicities in the app, in any part of the templates.

--- quicktutorsApp/context_processors.py
+++ quicktutorsApp/context_processors.py
@@ -1,11 +1,13 @@
 from user_profile.models import UserProfile
 from django.contrib.auth.decorators import login_required
+from monitorias.models import Publicity
 
 
 
 def quicktutorsApp(request):
     if request.user.is_authenticated():
         userProfile = UserProfile.objects.get(user=request.user)
-        return {'USER_PROFILE': userProfile, 'ESTADO_PENDIENTE': "aceptada"}
+        publicity_list = Publicity.objects.all()
+        return {'USER_PROFILE': userProfile, 'PUBLICITY_LIST': publicity_list}
     else:
         return {'USER_PROFILE': "None"}
